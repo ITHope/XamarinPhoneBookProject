e02e5da0ba6cd74a9fd6caec722a3d6b5c675e5e
Berdnyk Nadiia <admin@Mac-Admin.local>
2018-01-16 12:21:05 +0200

    Refactoring iOs

--- PhoneList/PhoneList.iOS/CollectionViewCell.cs
+++ PhoneList/PhoneList.iOS/CollectionViewCell.cs
@@ -33,40 +33,8 @@ namespace PhoneList.iOS
 
             var delta = (nfloat)(1 - ((featuredHeight - Frame.Height) / (featuredHeight - standardHeight)));
 
-
-            //var minAlpha = (nfloat)0.7;
-            //var maxAlpha = (nfloat)1.0;
-            //_imgIcon.Alpha = maxAlpha - (delta * (maxAlpha - minAlpha));
-            nfloat scale = 1;// = (nfloat)Math.Max(delta, 0.5);
-            if (delta > 0.5)
-            {
-                scale = 2;
-            }
-
-            //_imgIcon.Transform = CGAffineTransform.MakeScale(scale, scale);
-
-            //_imgIconTopConstraint.Constant = delta * 50;
-            //_imgIconLeftConstraint.Constant = delta * 50;
             _imgIconBottomConstraint.Constant = delta * 70;
             _lblFNameTopConstrraint.Constant = delta * 50;
-            //_lblPhoneTopConstraint.Constant = delta * 100;
-
-            //_cellView.Layer.BorderWidth = 1;
-            //_cellView.Layer.BorderColor = UIColor.Gray.CGColor;
-
-            //if (_cellView != null)
-            //{
-            //    CALayer borderLayer = new CALayer();
-            //    borderLayer.BackgroundColor = UIColor.FromRGB(23, 162, 227).CGColor;
-            //    borderLayer.Frame = new CGRect(0, Frame.Height, 335, 5);
-            //    Layer.AddSublayer(borderLayer);
-            //}
-
-            //_lblFName.Transform = CGAffineTransform.MakeScale(scale, scale);
-            //_lblLName.Transform = CGAffineTransform.MakeScale(scale, scale);
-            //UIView.BeginAnimations("OpenInfo");
-            //UIView.SetAnimationDuration(1.0);
-            //_imgTopConstr.Constant = 100;
         }
 
         private void DrawBorder()
